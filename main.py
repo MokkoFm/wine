4cc9b494fa58033dca25b88461af4eecdda79af5
MokkoFm <48250531+MokkoFm@users.noreply.github.com>
2020-03-28 21:08:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+import datetime
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('index.html')
+first_year = datetime.datetime(year=1920, month=1, day=1, hour=0)
+today = datetime.datetime.now()
+delta = today.year - first_year.year
+
+rendered_page = template.render(
+    age=delta,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
