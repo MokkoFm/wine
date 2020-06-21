ca8cc640113baed1b1979fc14e78eb143d067116
MokkoFm <mokko2070@mail.ru>
2020-06-22 01:38:03 +0300

    Automated display of product catalog

--- main.py
+++ main.py
@@ -1,65 +1,32 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
 import datetime
 import pandas
+import xlrd
+import pprint
+from collections import defaultdict
 
-data = pandas.read_excel(r'C:\Users\mokko\Desktop\wine-master\wine.xlsx', na_values='', keep_default_na=False, usecols=['Название', 'Цена', 'Картинка', 'Сорт'], sheet_name='wines')
-print(data)
+file_with_wines = pandas.read_excel('offer.xlsx', sheet_name='wines', usecols=['category', 'title', 'type', 'price', 'image', 'discount'])
+wines_nomenclature = file_with_wines.to_dict(orient='record')
+wines_catalog = defaultdict(list)
+
+for wine in wines_nomenclature:
+  wines_catalog[wine['category']].append(wine)
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-template = env.get_template('index.html')
+template = env.get_template('template.html')
 first_year = datetime.datetime(year=1920, month=1, day=1, hour=0)
 today = datetime.datetime.now()
 delta = today.year - first_year.year
 
-wines = [
-    {
-        "title": data['Название'].tolist()[0],
-        "price": data['Цена'].tolist()[0],
-        "image": data['Картинка'].tolist()[0],
-        "type": data['Сорт'].tolist()[0]
-    },
-    {
-        "title": data['Название'].tolist()[1],
-        "price": data['Цена'].tolist()[1],
-        "image": data['Картинка'].tolist()[1],
-        "type": data['Сорт'].tolist()[1]
-    },
-    {
-        "title": data['Название'].tolist()[2],
-        "price": data['Цена'].tolist()[2],
-        "image": data['Картинка'].tolist()[2],
-        "type": data['Сорт'].tolist()[2]
-    },
-    {
-        "title": data['Название'].tolist()[3],
-        "price": data['Цена'].tolist()[3],
-        "image": data['Картинка'].tolist()[3],
-        "type": data['Сорт'].tolist()[3]
-    },
-    {
-        "title": data['Название'].tolist()[4],
-        "price": data['Цена'].tolist()[4],
-        "image": data['Картинка'].tolist()[4],
-        "type": data['Сорт'].tolist()[4]
-    },
-    {
-        "title": data['Название'].tolist()[5],
-        "price": data['Цена'].tolist()[5],
-        "image": data['Картинка'].tolist()[5],
-        "type": data['Сорт'].tolist()[5]
-    },
-]
-
 rendered_page = template.render(
     age=delta,
-    wines=wines,
+    wines_catalog=wines_catalog
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
